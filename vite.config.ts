d579fe4f2c389deab3b1168f14cd2b1a0451e70e
fracabu <100199965+fracabu@users.noreply.github.com>
2024-12-03 09:12:43 +0100

    remove point

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Assicurati che i percorsi relativi funzionino correttamente
+  base: '/', // Assicurati che i percorsi relativi funzionino correttamente
   build: {
     outDir: 'dist', // Directory di output per la build
   },
