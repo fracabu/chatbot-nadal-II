a0f81301936fae387da8e881ed2177186cc69939
fracabu <100199965+fracabu@users.noreply.github.com>
2024-12-03 02:00:09 +0100

    fix deploy vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'], // Escludi pacchetti specifici, se necessario
-  },
-  base: './', // Configura il base path per i file relativi
+  base: './', // Assicurati che i percorsi relativi funzionino correttamente
   build: {
     outDir: 'dist', // Directory di output per la build
-    assetsDir: 'assets', // Directory per i file statici
-  },
-  server: {
-    port: 5173, // Assicurati che la porta sia corretta
-    open: true, // Apre automaticamente il browser
   },
 });
