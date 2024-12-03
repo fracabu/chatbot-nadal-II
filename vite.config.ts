3c0194f43c65086a632776b4e6d98db95730ce77
fracabu <100199965+fracabu@users.noreply.github.com>
2024-12-03 09:27:55 +0100

    remove svg change url

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Assicurati che i percorsi relativi funzionino correttamente
+  base: './', // Cambia il base path per percorsi relativi
   build: {
-    outDir: 'dist', // Directory di output per la build
+    outDir: 'dist',
   },
 });
